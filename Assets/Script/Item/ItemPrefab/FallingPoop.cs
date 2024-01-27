03e77097ff515e6d041d795818e6408a72579f02
stephenwong1997 <14219328@life.hkbu.edu.hk>
2024-01-27 20:53:27 +0800

    Poop Ground

---
+++ Assets/Script/Item/ItemPrefab/FallingPoop.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FallingPoopItem : IEquippedItem
+{
+    private FallingPoop _fallingPoopPrefab;
+    public FallingPoopItem(FallingPoop fallingPoopPrefab)
+    {
+        _fallingPoopPrefab = fallingPoopPrefab;
+    }
+    public EItemType GetItemType()
+    {
+        return EItemType.Poop;
+    }
+
+    public void Use(IItemContext context)
+    {
+        Vector2 fireDirection = context.IsFacingLeft ? Vector2.left + Vector2.up : Vector2.one;
+        SpawnPoop(context.SpawnPosition);
+    }
+    private void SpawnPoop(Vector2 spawnPosition)
+    {
+        FallingPoop poopInstance = GameObject.Instantiate(_fallingPoopPrefab);
+        poopInstance.transform.position = spawnPosition;
+    }
+}
+[RequireComponent(typeof(Rigidbody2D))]
+public class FallingPoop : MonoBehaviour, IItemPrefab
+{
+    private Rigidbody2D _rigidbody;
+    [SerializeField] private GameObject poopGround;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(poopGround, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
